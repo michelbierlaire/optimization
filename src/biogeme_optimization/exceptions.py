4af35451af047714742bd9d9db238befdb8f298d
Michel Bierlaire <michel.bierlaire@epfl.ch>
2023-06-24 18:10:53 +0200

    Create exceptions.py

---
+++ src/biogeme_optimization/exceptions.py
@@ -0,0 +1,11 @@
+"""Defines a generic exception for the optimization package
+
+:author: Michel Bierlaire
+
+:date: Wed Jun 21 10:34:51 2023
+
+"""
+
+
+class OptimizationError(Exception):
+    """Defines a generic exception ."""
